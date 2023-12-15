15f1edbfb0f31d6bde7069c43240cd27156f1b9d
Sung <sungjincho92@gmail.com>
2023-12-14 22:39:58 -0300

    third data fill attempt

---
+++ js/script2.js
@@ -0,0 +1,24 @@
+const perceptions = document.querySelector('.perceptions');
+
+const generateMarkup = function(data) {
+    return data.map( el => {
+        return `
+        <div class="per-wrap per-${el.category.toLowerCase()}">
+        <div class="per-flex-helper">
+             <img src="${el.icon}">
+             <p class="per-${el.category.toLowerCase()}-c">${el.category}</p>
+         </div>
+        <p class="per-score">${el.score}<span> / 100</span></p>
+        </div>
+        `
+    }).join('')
+};
+
+
+const htmlTemplate = function() {
+    fetch("./../data.json")
+    .then(res => res.json())
+    .then(data => console.table(data));
+}
+
+htmlTemplate();
