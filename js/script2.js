5faa8bdbb981db22cd71a438c54011d10966ea1a
Sung <sungjincho92@gmail.com>
2023-12-16 12:04:12 -0300

    minor fixes

--- js/script2.js
+++ js/script2.js
@@ -1,14 +1,12 @@
-const fs = require('fs');
 const perceptions = document.querySelector('.perceptions');
-const data = fs.readFileSync("data.json", 'utf-8');
-const dataObj = JSON.parse(data);
+
 
 const generateMarkup = function(data) {
     return data.map( el => {
         return `
         <div class="per-wrap per-${el.category.toLowerCase()}">
         <div class="per-flex-helper">
-             <use xlink:href=${el.icon}></use>
+             <img src=${el.icon}>
              <p class="per-${el.category.toLowerCase()}-c">${el.category}</p>
          </div>
         <p class="per-score">${el.score}<span> / 100</span></p>
@@ -17,7 +15,14 @@ const generateMarkup = function(data) {
     }).join('')
 };
 
-const markup = generateMarkup(dataObj);
+const htmlTemplate = async function() {
+    const data = await fetch("data.json");
+    const result = await data.json();
+    return result;
+}
 
-console.log(markup);
+const template = await htmlTemplate();
+
+const markup = generateMarkup(template);
+
 perceptions.insertAdjacentHTML('afterbegin', markup);
